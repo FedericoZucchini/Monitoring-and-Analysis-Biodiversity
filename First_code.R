01f4c8dab39028e825005ab61a355e6cd8df5c76
Federico Zucchini <162352694+FedericoZucchini@users.noreply.github.com>
2025-05-06 12:27:33 +0200

    Create First_code.R

---
+++ First_code.R
@@ -0,0 +1,12 @@
+#Installation packages from CRAN
+#Installation package "terra"
+install.packages ("terra") ## Si mettono le virgolette perchè è una parte testuale
+## Selezionare una città da cui scaricare il pacchetto. Si usa solitamente Padova.
+#Usare comando "library" per vedere se il pacchetto è stato installato correttamente.
+library (terra) ## Si usa in R quindi non si usano le virgolette #package used for remote sensing data
+
+#Importing data
+#Mac o Linux: option5 porta a creare tilde (option5/Desktop/)
+#Per Windows
+setwd ("C://utente/Desktop/") # Serve per settare la cartella di lavoro
+# ATTENZIONE! USE SLASH AND NOT BACKSLASH
