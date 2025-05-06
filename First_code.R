38d8bd771c2e37ec6d2f96280f70f96c8945d68b
Federico Zucchini <162352694+FedericoZucchini@users.noreply.github.com>
2025-05-06 12:46:45 +0200

    Update First_code.R

--- First_code.R
+++ First_code.R
@@ -10,3 +10,16 @@ library (terra) ## Si usa in R quindi non si usano le virgolette #package used f
 #Per Windows
 setwd ("C://utente/Desktop/") # Serve per settare la cartella di lavoro
 # ATTENZIONE! USE SLASH AND NOT BACKSLASH
+
+mato = rast(Matogrosso_l5_1992219_lrg.jpg)
+plot(mato)
+
+#Bands:
+#Band 1: nir
+#Band 2: red
+#Band 3: green
+
+plotRGB (mato, r=2, g=3, b=1)
+plotRGB (mato, r=2, g=1, b=3)
+plotRGB (mato, r=1, g=2, b=3)
+
